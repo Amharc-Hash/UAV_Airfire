5d8451d882c6177da5f93d7137b704c5a89c1d42
sinewc <69142166+sinewc@users.noreply.github.com>
2024-03-01 22:38:58 +0700

    add file

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="app">
+		<SideBar />
+		<router-view />
+	</div>
+</template>
+
+<script setup>
+import SideBar from './components/SideBar.vue';
+</script>
+<style>
+:root {
+	--primary: #FF8202;
+	--primary-alt: #FF8202;
+	--grey: #BFBFBF;
+	--dark: #1E1E1E;
+	--dark-alt: #0E0E0E;
+	--light: #f1f5f9;
+	--sidebar-width: 260px;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira sans', sans-serif;
+}
+
+body {
+	background-color: var(--grey);
+}
+
+button {
+	cursor: pointer;
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
+}
+
+.app {
+	display: flex;
+
+	main {
+		flex: 1 1 0;
+		padding: 2rem;
+
+		@media (max-width: 1024px) {
+			padding-left: 6rem;
+		}
+	}
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Overview from '../views/Overview.vue'
+
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: Overview
+		},
+		{
+			path: '/fire-Analysis',
+			component: () => import('../views/FireAnalysis.vue')
+		},
+	],
+})
+
+export default router
