61b7b4350234c912ffd4ba18f4a92d1c2d66460f
Amharc-Hash <vachara.vr@gmail.com>
2024-03-05 04:16:51 +0700

    Add code read sensor and send it to website

--- server.py
+++ server.py
@@ -1,21 +1,73 @@
 from flask import Flask, jsonify
 import random  # For simulating real-time data updates
 import time
+from flask_cors import CORS  # Import CORS
+import json
+import serial
+from pymodbus.client.sync import ModbusSerialClient
+import cv2
+import base64
+import asyncio
+import websockets
 
 app = Flask(__name__)
+CORS(app)
 
 # Replace this with your actual data fetching/generation logic
-def get_data():
+def get_sensor_data():
+    data_list = []
+    wind_dir = ['S','SW','W','NW','N','NE','E','SE']
+
+    # RS485 USB Configuration
+    port = '/dev/ttyUSB0'  # Replace if your USB adapter uses a different port
+    baudrate = 9600        # Adjust if your device uses a different baud rate
+    parity = 'N'
+    stopbits = 1
+    bytesize = 8
+
+    # Modbus Slave Configuration
+    client = ModbusSerialClient(method='rtu', port=port, baudrate=baudrate,
+                                parity=parity, stopbits=stopbits, bytesize=bytesize,
+                                timeout=2000)   # Adjust timeout if needed
+
+    slave_id = 0x02           # ID of your Modbus slave device
+    register_address_list = [0x1F4, 0x1F6, 0x1F8, 0x1F9]    # [WindSpeed, WindDir, Hum, Temp]
+    count = 1               # Number of registers to read
+
+    # Connect to the device
+    client.connect()
+
+    # Read data
+    for register_address in register_address_list:
+        result = client.read_holding_registers(register_address, count, unit=slave_id)
+        if not result.isError():
+            data_list.append(result.registers[0])
+            #print(len(result.registers))
+        else:
+            data_list.append('error')
+            print(f'Error reading registers : {register_address}: {result}')
+
+    # Process the result
+    print(f'Wind Speed: {data_list[0]/100} m/s')
+    print(f'Wind Direction: {wind_dir[data_list[1]]}')
+    print(f'Humidity: {data_list[2]/10} %')
+    print(f'Temperature: {data_list[3]/10} C')
+    # Close the connection
+    client.close()
+
+    # Create a dictionary for easy use in Vue
     return {
-        'valueTemp': random.randint(28, 35),
-        'windSpeed': random.randint(1, 10),
-        'windDirec': ['N', 'S', 'E', 'W'][random.randint(0, 3)],
-        'humidity':  random.randint(40, 80)
+        'valueTemp': data_list[3]/10,
+        'windSpeed': data_list[0]/100,
+        'windDirec': wind_dir[data_list[1]],
+        'humidity':  data_list[2]/10
     }
 
 @app.route('/weather_data')
 def weather():
-    return jsonify(get_data())
+    response = jsonify(get_sensor_data())
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:5173')
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  
+    app.run(debug=True, port=5000)
